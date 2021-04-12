4afcaa936136e69ff66253ef912aba09c791827f
Mariano Shaar <shaargtz@3030.local>
2021-04-12 18:22:10 -0500

    se creo la estructura del menu

--- src/menu.c
+++ src/menu.c
@@ -1,7 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 int main(int argc, char *argv[]) {
+    bool menu = true;
+    int choice;
 
+    while (menu) {
+        printf("Selecciona una opcion:\n1 Mostrar estudiantes\n2 Hacer una query\n3 Salir\n");
+        scanf("%d", &choice);
+        switch (choice) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                menu = false;
+                break;
+        }
+    }
     return 0;
 }
