4d12328de09aa1aecaf8ac67e241af4fc2bc3ef4
Mariano Shaar <a00825287@itesm.mx>
2021-04-12 20:41:57 -0500

    se empezo el esqueleto de ejecutar las queries

--- lib/queries/queries.c
+++ lib/queries/queries.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 leeQuery() {
     char *buffer;
@@ -8,4 +9,45 @@ leeQuery() {
     printf("Escribe tu query:\n");
     getline(&buffer, &bufsize, stdin);
     ejecutaQuery(buffer);
+}
+
+ejecutaQuery(char *query) {
+    char *arg1 = strtok(query, " ");
+    if (arg1 == "Kardex") {
+        char *arg2 = strtok(query, " ");
+        if (!arg2) printf("Query invalida");
+        else {
+            
+        }
+    } else if (arg1 == "Fecha_estimada_graduacion") {
+        char *arg2 = strtok(query, " ");
+        if (!arg2) printf("Query invalida");
+        else {
+            
+        }
+    } else if (arg1 == "Numero_alumnos") {
+        char *arg2 = strtok(query, " ");
+        if (!arg2) printf("Query invalida");
+        else {
+            char *arg3 = strtok(query, " ");
+            if (!arg2) {
+
+            } else {
+                
+            }
+        }
+    } else if (arg1 == "Nombre_alumnos") {
+        char *arg2 = strtok(query, " ");
+        if (!arg2) printf("Query invalida");
+        else {
+            char *arg3 = strtok(query, " ");
+            if (!arg2) {
+
+            } else {
+                
+            }
+        }
+    } else {
+        printf("Query invalida");
+    }
 }
