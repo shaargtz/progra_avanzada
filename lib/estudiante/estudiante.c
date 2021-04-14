40db8a3b0c7e0d98c14cc56e9eb581112340c767
Mariano Shaar <a00825287@itesm.mx>
2021-04-13 21:07:01 -0500

    se corrigio la manera en que se imprimen los datos de los estudiantes

--- lib/estudiante/estudiante.c
+++ lib/estudiante/estudiante.c
@@ -18,15 +18,15 @@ void muestra(Estudiante arr[], bool write, FILE *out, bool verbose, int size) {
     if (write) fprintf(out, "Todos los estudiantes\n");
     for (int i = 0; i < size; i++) {
         printf("ID: %i\tNombre: %s\tCarrera: %s\tCiudad: %s\tFecha de graduacion: %s\t"\
-            "Materia A: %i\tMateria B: %i\tMateria C: %i\tMateria D: %i\n", 
+            "Materia A: %i\tMateria B: %i\tMateria C: %i\tMateria D: %i\tPromedio: %.2f\n", 
             arr[i].id, arr[i].nombre, arr[i].carrera, arr[i].ciudad, arr[i].fecha,
-            arr[i].calificaciones[0], arr[i].calificaciones[1], arr[i].calificaciones[2], arr[i].calificaciones[3]);
+            arr[i].calificaciones[0], arr[i].calificaciones[1], arr[i].calificaciones[2], arr[i].calificaciones[3], arr[i].promedio);
         if (write) {
             if (verbose) printf("Escribiendo al archivo...\n");
-            fprintf(out, "Todos los estudiantes\nID: %i\tNombre: %s\tCarrera: %s\tCiudad: %s\tFecha de graduacion: %s\t"\
-                "Materia A: %i\tMateria B: %i\tMateria C: %i\tMateria D: %i\n",
+            fprintf(out, "ID: %i\tNombre: %s\tCarrera: %s\tCiudad: %s\tFecha de graduacion: %s\t"\
+                "Materia A: %i\tMateria B: %i\tMateria C: %i\tMateria D: %i\tPromedio: %.2f\n",
                 arr[i].id, arr[i].nombre, arr[i].carrera, arr[i].ciudad, arr[i].fecha,
-                arr[i].calificaciones[0], arr[i].calificaciones[1], arr[i].calificaciones[2], arr[i].calificaciones[3]);
+                arr[i].calificaciones[0], arr[i].calificaciones[1], arr[i].calificaciones[2], arr[i].calificaciones[3], arr[i].promedio);
         }
     }
 }
